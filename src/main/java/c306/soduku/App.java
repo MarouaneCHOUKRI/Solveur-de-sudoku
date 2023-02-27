b848960a6ea1d22fc81007ebe779f99e14febc5c
Marouane Choukri <choukrimarouane10@gmail.com>
2023-02-27 18:11:21 +0100

    Add files via upload

---
+++ src/main/java/c306/soduku/App.java
@@ -0,0 +1,13 @@
+package c306.soduku;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/c306/soduku/AppTest.java
@@ -0,0 +1,20 @@
+package c306.soduku;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
